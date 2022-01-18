b46f193feb3df0de6ca7012e53c4ea56c5a20654
zhanyd <zhanyd@sina.com>
2022-01-18 21:43:21 +0800

    add PaymentMain8001

---
+++ springcloudDemo/cloud-provider-payment8001/src/main/java/com/zhanyd/springcloud/PaymentMain8001.java
@@ -0,0 +1,14 @@
+package com.zhanyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhan
+ */
+@SpringBootApplication
+public class PaymentMain8001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain8001.class,args);
+    }
+}
