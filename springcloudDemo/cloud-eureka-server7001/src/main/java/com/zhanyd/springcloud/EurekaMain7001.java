085208ae9ccea799263a67f32796b70d5a927c45
zhanyd <zhanyd@sina.com>
2022-03-06 21:19:41 +0800

    add eureka

---
+++ springcloudDemo/cloud-eureka-server7001/src/main/java/com/zhanyd/springcloud/EurekaMain7001.java
@@ -0,0 +1,14 @@
+package com.zhanyd.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
