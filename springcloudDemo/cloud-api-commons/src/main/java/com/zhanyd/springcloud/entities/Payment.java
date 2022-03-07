9ae70ad664577006110c65ba679ac4fd032b9bdd
zhanyd <zhanyd@sina.com>
2022-03-07 21:00:21 +0800

    add

--- springcloudDemo/cloud-api-commons/src/main/java/com/zhanyd/springcloud/entities/Payment.java
+++ springcloudDemo/cloud-api-commons/src/main/java/com/zhanyd/springcloud/entities/Payment.java
@@ -1,2 +1,15 @@
-package com.zhanyd.springcloud.entities;public class Payment {
+package com.zhanyd.springcloud.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+    private Long id;
+    private String serial;
 }
